98223e985aa958815e3b8d8d1c2e19d7f04ff8b5
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-03-08 10:45:34 -0300

    style: add GlobalReset and GlobalStyle

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GlobalReset } from './styles/GlobalReset';
+import { GlobalStyles } from './styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalReset />
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
