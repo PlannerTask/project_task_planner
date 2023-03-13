afb9844c2f85c21a76089e79eaba22427f99054c
João Paulo Rodrigues <rodrigues.jpfa@gmail.com>
2023-03-13 17:15:22 -0300

    fix: add css responsiveness input search

--- src/components/Search/style.ts
+++ src/components/Search/style.ts
@@ -6,15 +6,25 @@ export const StyledSearch = styled.form`
   justify-content: center;
   margin-top: 22px;
   input {
-    width: 200px;
-    height: 23px;
+    width: 165px;
+    height: 18px;
 
     color:var(--color-white);
-    background: #252a34;
+    background-color: var(--color-grey-600);
     border-radius: 8px 0 0 8px;
-    border: none;
+    border: 1px solid var(--color-grey-600);
 
     padding: 10px;
+
+    outline: none;
+
+    &:hover {
+      border: 1px solid var(--color-primary-hover);
+    }
+
+    &:focus {
+      border: 1px solid var(--color-primary-hover);
+    }
   }
   .button-search {
     display: flex;
@@ -23,17 +33,35 @@ export const StyledSearch = styled.form`
     padding: 0;
   }
   .icon {
-    width: 40px;
-    height: 33px;
+    width: 32px;
+    height: 30px;
     padding: 5px;
 
-    background: #c5ff75;
+    background-color:var(--color-primary);
     mix-blend-mode: normal;
     border-radius: 0px 8px 8px 0px;
+
+    &:hover {
+      background-color: var(--color-primary-hover);
+    }
   }
-  @media (min-width:768px) {
+  @media (min-width:769px) {
+
+    input {
+    width: 200px;
+    height: 23px;
+
+  }
+
     .button-search {
       cursor: pointer;
+  }
+
+  .icon {
+    width: 40px;
+    height: 35px;
+    padding: 5px;
+
   }
   }
 `;
